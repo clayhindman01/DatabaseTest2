fa36dad8fc5d3aed10b903eb00bc35a728ae3df6
clayhindman01 <clayhindman01@gmail.com>
2021-11-11 16:44:23 -0500

    Finished the assignment

--- viewActors.php
+++ viewActors.php
@@ -25,7 +25,7 @@ $mysqli -> set_charset("utf8mb4");
 //There is no info to get from the $_POST array
 
 //Run the sql script for the actor table
-$stmt_actor = $mysqli -> prepare("SELECT last_name, first_name FROM actor order by actor.last_name");
+$stmt_actor = $mysqli -> prepare("select a.last_name, a.first_name, a.actor_id, f.title from actor a inner join film_actor fa on a.actor_id = fa.actor_id inner join film f on f.film_id = fa.film_id order by last_name, first_name");
 $stmt_actor -> execute();
 
 $result_actor = $stmt_actor -> get_result();
@@ -36,12 +36,36 @@ print("<table border='1'>");
 print("<tr> <th>Last Name</th> <th>First Name</th> <th>Movie Count</th> <th>List of Movies</th></tr>");
 
 $i = 0;
+$full_data = [];
 while($row = $result_actor -> fetch_assoc()) {
-    $actor_names[$i][] = $row['first_name'];
-    $actor_names[$i][] = $row['last_name'];
-    print ("<tr><td>".$row['last_name']."</td><td>".$row['first_name']."</td></tr>");
-    $i++;
+    $first_name = $row['first_name'];
+    $last_name = $row['last_name'];
+    $title = $row["title"];
+    $actor_id = $row["actor_id"];
+
+    if(!array_key_exists("$last_name"."$first_name", $full_data)) {
+        //does not exist - so initialize
+        $full_data["$last_name"."$first_name"][0] = $first_name;
+        $full_data["$last_name"."$first_name"][1] = $title;
+        $full_data["$last_name"."$first_name"][2] = 1;
+        $full_data["$last_name"."$first_name"][3] = $last_name;
+    } else {
+        //already exists - so concat city name, increment count
+        $full_data["$last_name"."$first_name"][1] .= ",".$title;
+        $full_data["$last_name"."$first_name"][2] += 1;
+    }
 }
+
+foreach($full_data as $last_name=>$values) {
+    //$values[0] is cities as a string
+    //$values[1] is count of number of cities
+    $first_name = $values[0];
+    $count = $values[2];
+    $titles = $values[1];
+    $last_name = $values[3];
+    print("<tr><td>$last_name</td><td>$first_name</td><td>$count</td><td>$titles</td></tr>");
+}
+
 
 print("<form action='tasks.html'><input type='submit' value='Back To Home Page' /></form>");
 
--- viewRentals.php
+++ viewRentals.php
@@ -25,7 +25,15 @@ $mysqli -> set_charset("utf8mb4");
 //There is no info to get from the $_POST array
 
 //Run the sql script for the actor table
-$stmt = $mysqli -> prepare("SELECT title, description, rental_duration, rental_rate, length, special_features, name FROM film f, category c, film_category fc WHERE f.film_id = fc.film_id AND c.category_id = fc.category_id");
+$stmt = $mysqli -> prepare("select title, description, rental_duration, rental_rate, length, special_features, name, amount FROM film f 
+inner join film_category fc on fc.film_id = f.film_id 
+inner join category c on fc.category_id = c.category_id
+inner join inventory i ON f.film_id = i.film_id 
+inner join rental r on r.inventory_id = i.inventory_id 
+inner join payment p on p.rental_id = r.rental_id 
+order by title;");
+
+
 $stmt -> execute();
 
 $result = $stmt -> get_result();
@@ -36,18 +44,48 @@ print("<table border='1'>");
 print("<tr> <th>Title</th> <th>Description</th> <th>Rental Duration</th> <th>Rental Rate</th> <th>Length</th> <th>Special Features</th> <th>Category</th> <th>Value</th> </tr>");
 
 $i = 0;
+$full_data = array();
+$sum = 0;
 while($row = $result -> fetch_assoc()) {
-    $film[$i][] = $row['title'];
-    $film[$i][] = $row['description'];
-    $film[$i][] = $row['rental_duration'];
-    $film[$i][] = $row['rental_rate'];
-    $film[$i][] = $row['length'];
-    $film[$i][] = $row['special_features'];
-    $film[$i][] = $row['name'];
+    $title = $row['title'];
+    $description = $row['description'];
+    $rental_duration = $row['rental_duration'];
+    $rental_rate = $row['rental_rate'];
+    $length = $row['length'];
+    $special_features = $row['special_features'];
+    $name = $row['name'];
+    $amount = $row['amount'];
 
+    if(!array_key_exists($title, $full_data)) {
+        //does not exist - so initialize
+        $full_data[$title][0] = $title;
+        $full_data[$title][1] = $description;
+        $full_data[$title][2] = $rental_duration;
+        $full_data[$title][3] = $rental_rate;
+        $full_data[$title][4] = $length;
+        $full_data[$title][5] = $special_features;
+        $full_data[$title][6] = $name;
+        $full_data[$title][7] = $amount;
+        $sum = $amount;
+    } else {
+        $full_data[$title][7] = $sum + $amount;
+        $sum = $sum + $amount;
 
-    print ("<tr> <td>".$row['title']."</td> <td>".$row['description']."</td> <td>".$row['rental_duration']."</td> <td>".$row['rental_rate']."</td> <td>".$row['length']."</td> <td>".$row['special_features']."</td> <td>".$row['name']."</td> </tr>");
+    }
     $i++;
+}
+
+foreach($full_data as $title=>$values) {
+    $title = $values[0];
+    $description = $values[1];
+    $rental_duration = $values[2];
+    $rental_rate = $values[3];
+    $length = $values[4];
+    $special_features = $values[5];
+    $name = $values[6];
+    $amount = $values[7];
+    
+    print ("<tr> <td>$title</td><td>$description</td> <td>$rental_duration</td> <td>$rental_rate</td> <td>$length</td> <td>$special_features</td> <td>$name</td> <td>$amount</td> </tr>");
 }
 
 print("<form action='tasks.html'><input type='submit' value='Back To Home Page' /></form>");
